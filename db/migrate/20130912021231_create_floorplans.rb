a042e02be3b303aca225495ce613f45ae13b7462
Taylor Lapeyre <taylorlapeyre@gmail.com>
2013-09-11 21:12:37 -0500

    add floorplan model

---
+++ db/migrate/20130912021231_create_floorplans.rb
@@ -0,0 +1,20 @@
+class CreateFloorplans < ActiveRecord::Migration
+  def change
+    create_table :floorplans do |t|
+      t.integer :listing_id
+      t.string :name
+      t.integer :num_bathrooms
+      t.integer :num_bedrooms
+      t.integer :rent
+      t.string :rent_type
+      t.integer :square_footage
+      t.integer :lease_term
+      t.integer :deposit
+      t.integer :lease_term
+      t.string :lease_type
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
