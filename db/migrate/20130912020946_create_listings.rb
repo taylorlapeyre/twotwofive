b8d6d8999a35ebc0adee12aeaafbeea6d9cf0ce5
Taylor Lapeyre <taylorlapeyre@gmail.com>
2013-09-11 23:15:18 -0500

    change name to building_name

--- db/migrate/20130912020946_create_listings.rb
+++ db/migrate/20130912020946_create_listings.rb
@@ -1,7 +1,7 @@
 class CreateListings < ActiveRecord::Migration
   def change
     create_table :listings do |t|
-      t.string :name
+      t.string :building_name
       t.string :address
       t.string :zip
       t.text :descrption
