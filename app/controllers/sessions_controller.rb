baabf1631b33d5251577fe3d80c80d226cc5e33e
Taylor Lapeyre <taylorlapeyre@gmail.com>
2013-09-24 20:17:34 -0500

    fixing bugs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		landlord = Landlord.find_by(email: params[:session][:email].downcase)
+		landlord = Landlord.find_by(email: params[:session][:email].downcase, password: params[:session][:password])
 
-		if landlord && landlord.authenticate(params[:session][:password])
+		if landlord # && landlord.authenticate(params[:session][:password])
 			sign_in landlord
       redirect_to root_path
 		else
