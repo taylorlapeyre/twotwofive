e0730e2490f558d64045b3c7819129fb9b2343a0
Taylor Lapeyre <taylorlapeyre@gmail.com>
2013-09-30 22:54:48 -0500

    fix typo

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
 	belongs_to :listing
 	belongs_to :floorplan
 
-	has_attached_file :image_file, :styles => { :small => "400x400", :medium => "600x600", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image_file, :styles => { :small => "400x400", :medium => "600x600", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
 
 	validates_attachment_presence :image_file
 end
