fc679c3c2be516b766c5333270425f7fd9b3ed6a
Taylor Lapeyre <taylorlapeyre@gmail.com>
2013-10-14 22:06:09 -0500

    fix bug

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,16 @@
 class SearchController < ApplicationController
   def create
+  	# TODO: Clean up this code
+
   	@listings = []
 
   	Listing.all.each do |listing|
   		listing.floorplans.each do |floorplan|
-  			if params[:num_bedrooms] == "4+" && params[:num_bathrooms] != "3+"
+  			if params[:num_bedrooms] == "4+" && params[:num_bathrooms] == "3+"
+  				if floorplan.num_bedrooms > params[:num_bedrooms].to_i && floorplan.num_bathrooms > params[:num_bathrooms].to_i
+  					@listings << listing
+  				end
+  			elsif params[:num_bedrooms] == "4+" && params[:num_bathrooms] != "3+"
   				if floorplan.num_bedrooms > params[:num_bedrooms].to_i && floorplan.num_bathrooms == params[:num_bathrooms].to_i
   					@listings << listing
   				end
